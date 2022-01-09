b83895ca598fc1338c0d43abb70f60412d19bea8
skyleaworlder <870033938@qq.com>
2022-01-09 13:38:58 +0800

    add: const.js 不知道未来能不能用到

---
+++ src/const.js
@@ -0,0 +1,14 @@
+const MODULE_TYPE = Object.freeze({
+  cc_binary: 0,
+  cc_library: 1,
+  cc_library_headers: 2,
+  cc_library_host_shared: 3,
+  cc_library_shared: 4,
+  cc_library_static: 5,
+  java_library: 6,
+  java_sdk_library: 7,
+  ndk_headers: 8,
+  ndk_library: 9,
+  prebuilt_etc: 10,
+  python_binary_host: 11
+})
